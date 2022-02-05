1486be58335ad1d1f5b6cf90a6746ac1e0c5432e
viveknathani <viveknathani2402@gmail.com>
2022-02-05 15:23:40 +0530

    chore: serve the React app

--- server.ts
+++ server.ts
@@ -3,43 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import { QueryResult } from 'pg';
-import { setupDatabase, queryWithTransaction } from './database';
-import { setupCache, getClient } from './cache';
-
-const app: express.Application = express();
-
-app.get('/', async (req: express.Request, res: express.Response) => {
-
-    try {
-
-        let message = '';
-        message += '<p>This is the scrawl server.<p>';
-
-        await queryWithTransaction('SELECT NOW()', (rows: QueryResult<any>): Error | undefined => {
-
-            console.log(rows);
-            return undefined;
-        });
-
-        message += '<p>I can talk to the database server.</p>';
-
-        const client = getClient();
-        if (!client.isOpen) {
-            await client.connect();
-        }
-        await client.set('key', 'value');
-        message += '<p>I can talk to the cache server.</p>';
-
-        res.contentType('html');
-        res.send(message);
-    } catch (err) {
-
-        console.log(err);
-        res.send('oops! check logs.');
-    }
-});
+import { setupDatabase } from './database';
+import { setupCache } from './cache';
 
 setupDatabase(process.env.DATABASE_URL || '');
 setupCache(process.env.REDIS_URL || '', process.env.REDIS_USER || '', process.env.REDIS_PWD || '');
+
+const app: express.Application = express();
+
+// Serve the React App
+app.use(express.static('web/build')); 
 app.listen(process.env.PORT, () => console.log("Server is up!"));
