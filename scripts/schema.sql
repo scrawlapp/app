1750ea3f95f9f92f5f17b24fe953783e694de212
viveknathani <viveknathani2402@gmail.com>
2022-02-06 18:07:58 +0530

    fix: column name in Postgres is all lowercase
    by default, PostgreSQL turns the whole column name into lowercase.
    to preservethe exactness, we need to enclose the column names in double
    quotes.

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -2,8 +2,8 @@
 -- no upper limit. "email" is stored in accordance with RFC 5321 and Errata 1690.
 create table if not exists users(
     id uuid primary key,
-    firstName varchar not null,
-    lastName varchar not null,
+    "firstName" varchar not null,
+    "lastName" varchar not null,
     email varchar(319) not null,
     password bytea not null
 );
@@ -17,14 +17,14 @@ create table if not exists pages(
 -- "ability" represents what the user is capable of doing with a page.
 -- common values could be "editor", "viewer".
 create table if not exists abilities(
-    pageId uuid references pages(id),
-    userId uuid references users(id),
+    "pageId" uuid references pages(id),
+    "userId" uuid references users(id),
     ability varchar not null 
 );
 
 create table if not exists blocks(
     id uuid primary key,
-    pageId uuid references pages(id),
+    "pageId" uuid references pages(id),
     tag varchar not null,
     html varchar not null,
     position integer not null,
