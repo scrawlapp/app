de25b8eeeb92fcb974409dea99f88e810b12e55e
viveknathani <viveknathani2402@gmail.com>
2022-02-08 21:42:50 +0530

    chore: describe how a Page looks like

--- entity/index.ts
+++ entity/index.ts
@@ -7,3 +7,11 @@ export interface User {
     email: string,
     password: string
 }
+
+// Structure of a Page in the application
+export interface Page {
+
+    id: string,
+    owner: string,
+    name: string
+}
