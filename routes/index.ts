2c93564c2ef5c0775eb7824960f15a91e43e8e8f
viveknathani <viveknathani2402@gmail.com>
2022-02-08 21:45:16 +0530

    feat: implement Page API

--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
-import{ userRouter, verifyTokenMiddleware } from './user';
+import { userRouter, verifyTokenMiddleware } from './user';
+import pageRouter from './page';
 
 const router: express.Router = express.Router();
 router.use('/user', userRouter);
 router.use(verifyTokenMiddleware);
+router.use('/page', pageRouter);
 export default router;
