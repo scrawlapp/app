03518947a68853deda3992ced906495b0b9a85b0
Ronak Malkan <ronakmalkan2062001@gmail.com>
2022-02-15 16:22:13 +0530

    style: Changed the signUp page styling

--- web/src/components/signUp/signUp.tsx
+++ web/src/components/signUp/signUp.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import SignUpHalf from "./signUpHalf";
-import SignInHalf from "./signInHalf";
+import SignUpForm from "./signUpForm";
+import SignInPrompt from "./signInPrompt";
 import "../../style/auth.css";
 
 function signUp(): JSX.Element{
 
    return(
       <div className="container">
-         <SignUpHalf/>
-         <SignInHalf/>
+         <SignUpForm/>
+         <SignInPrompt/>
       </div>
    );
 }
