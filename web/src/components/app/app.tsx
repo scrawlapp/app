d4f6965532d6e24985dc80b084059a633f6856fe
Ronak Malkan <ronakmalkan2062001@gmail.com>
2022-02-22 16:13:05 +0530

    feat: sign in page UI done

--- web/src/components/app/app.tsx
+++ web/src/components/app/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route} from 'react-router-dom';
 import Signup from "../signUp/signUp";
+import SignIn from "../signIn/signIn";
 
 function App(): JSX.Element {
   return (
     <Routes>
          <Route path="/" element={<Signup/>}/>
+         <Route path='/signIn' element={<SignIn/>}/>
     </Routes>
   );
 }
--- web/src/components/signUp/signInPrompt.tsx
+++ web/src/components/signUp/signInPrompt.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../../style/auth.css";
+import {Link} from "react-router-dom";
 
 function signInPrompt(): JSX.Element{
 
    return(
-      <div className="signInPart">
+      <div className="PromptContainer">
          <h1 className="title">Scrawl</h1>
          <h1>Already a user? Start scrawling</h1>
-         <button type="submit"><strong>Sign In</strong></button>
+         <Link className="auth_link" to="/SignIn"><strong>Sign In</strong></Link>
       </div>
    );
 }
