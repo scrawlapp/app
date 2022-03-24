efe01da89009e784c182b3a3b095f6df5537f8b7
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2022-03-24 23:59:43 +0530

    feat: added routes for home and settings page in app.tsx

--- web/src/components/app/app.tsx
+++ web/src/components/app/app.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Routes, Route} from 'react-router-dom';
 import Signup from "../signUp/signUp";
 import SignIn from "../signIn/signIn";
+import {Home} from "../home/home";
+import Settings from "../settings/settings";
 
 function App(): JSX.Element {
+  
   return (
     <Routes>
          <Route path="/" element={<Signup/>}/>
-         <Route path='/signIn' element={<SignIn/>}/>
+         <Route path='/signIn' element={<SignIn />}/>
+         <Route path="/home" element={<Home name=''/>}/>
+         <Route path='/settings' element={<Settings/>}/>
     </Routes>
   );
 }
