97c85fdf2df9e445c4911d4de61daaf049cc8e9e
viveknathani <viveknathani2402@gmail.com>
2022-03-27 17:37:35 +0530

    chore: use the block router

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
 import { userRouter, verifyTokenMiddleware } from './user';
 import pageRouter from './page';
+import blockRouter from './block';
 
 const router: express.Router = express.Router();
 router.use('/user', userRouter);
 router.use(verifyTokenMiddleware);
 router.use('/page', pageRouter);
+router.use('/block', blockRouter);
 export default router;
