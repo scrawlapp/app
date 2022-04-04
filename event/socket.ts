b6f180151370808c4d8a1f2072e3812e55c4a07c
viveknathani <viveknathani2402@gmail.com>
2022-04-04 16:29:37 +0530

    chore: setup a basic event to trigger page diffs

--- event/socket.ts
+++ event/socket.ts
@@ -1,8 +1,17 @@
 import { Server } from 'socket.io';
 
+interface PageDiff {
+
+    pageId: string,
+    blockId: string,
+    diff: string
+}
+
 export function attachEvents(io: Server) {
 
-    io.on('connection', (_) => {
-        console.log('a socket connected');
-    });
+    io.on('connection', (socket) => {
+        socket.on('page diff', (diff: PageDiff) => {
+            io.emit('page diff', diff);
+        });
+    })
 }
