bc1423470da96f82a915839aa57ac6f6552bf445
viveknathani <viveknathani2402@gmail.com>
2022-04-04 16:28:37 +0530

    feat: setup ability at the service layer

--- service/index.ts
+++ service/index.ts
@@ -1,4 +1,5 @@
 export { UserService } from './UserService';
 export { PageService } from './PageService';
 export { BlockService } from './BlockService';
+export { AbilityService } from './AbilityService';
 export * as errors from './errors';
