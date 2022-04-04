4517bcfe960e72b81dfde200e2b91d27606c41da
viveknathani <viveknathani2402@gmail.com>
2022-04-04 16:29:11 +0530

    feat: setup endpoints for /api/ability/

---
+++ routes/ability.ts
@@ -0,0 +1,71 @@
+import * as express from 'express';
+import { AbilityService, errors } from '../service/';
+import * as messages from './http_messages';
+
+const abilityRouter = express.Router();
+const abilityService = AbilityService.getInstance();
+
+/**
+ * @api {get} /api/ability/pages/ Get all shared pages
+ * @apiGroup Ability
+ * @apiName Get all shared pages
+ * @apiError (ServerError) {json} 500 Need to check server logs
+ * @apiVersion 0.1.0
+ * @apiDescription User needs to be authenticated to hit this endpoint.
+ */
+abilityRouter.get('/pages/', async (req: express.Request, res: express.Response) => {
+
+    try {
+        const pages = await abilityService.getAllSharedPages(req.body.userId);
+        res.status(200).json(pages);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: messages.MESSAGE_500 });
+    }
+});
+
+/**
+ * @api {post} /api/ability/ Insert an ability
+ * @apiGroup Ability
+ * @apiName Insert an ability
+ * @apiBody {string} pageId Mandatory
+ * @apiBody {string} userId Mandatory
+ * @apiBody {string} ability Mandatory
+ * @apiError (ServerError) {json} 500 Need to check server logs
+ * @apiVersion 0.1.0
+ * @apiDescription User needs to be authenticated to hit this endpoint.
+ */
+abilityRouter.post('/', async (req: express.Request, res: express.Response) => {
+
+    try {
+        await abilityService.insertAbility(req.body);
+        res.status(201).json({ message: messages.MESSAGE_201 });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: messages.MESSAGE_500 });
+    }
+});
+
+/**
+ * @api {delete} /api/ability/ Delete an ability
+ * @apiGroup Ability
+ * @apiName Delete an ability
+ * @apiBody {string} pageId Mandatory
+ * @apiBody {string} userIdToDelete Mandatory
+ * @apiError (ServerError) {json} 500 Need to check server logs
+ * @apiVersion 0.1.0
+ * @apiDescription User needs to be authenticated to hit this endpoint.
+ */
+abilityRouter.delete('/', async (req: express.Request, res: express.Response) => {
+
+    try {
+        const { pageId, userIdToDelete } = req.body;
+        await abilityService.deleteAbility(pageId, userIdToDelete);
+        res.status(200).json({ message: messages.MESSAGE_200 });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: messages.MESSAGE_500 });
+    }
+});
+
+export default abilityRouter;
--- routes/index.ts
+++ routes/index.ts
@@ -2,10 +2,12 @@ import * as express from 'express';
 import { userRouter, verifyTokenMiddleware } from './user';
 import pageRouter from './page';
 import blockRouter from './block';
+import abilityRouter from './ability';
 
 const router: express.Router = express.Router();
 router.use('/user', userRouter);
 router.use(verifyTokenMiddleware);
 router.use('/page', pageRouter);
 router.use('/block', blockRouter);
+router.use('/ability', abilityRouter);
 export default router;
