840e8121fa22f46321e76d670c59171f9154974c
amish kumar singh <amishkumarsingh0@gmail.com>
2022-04-13 12:49:56 +0530

    feat: completed the landing page

--- web/src/components/app/app.tsx
+++ web/src/components/app/app.tsx
@@ -4,12 +4,14 @@ import Signup from "../signUp/signUp";
 import SignIn from "../signIn/signIn";
 import {Home} from "../home/home";
 import Settings from "../settings/settings";
+import {LandingPage} from "../landingPage/landingPage";
 
 function App(): JSX.Element {
   
   return (
     <Routes>
-         <Route path="/" element={<Signup/>}/>
+         <Route path="/" element={<LandingPage/>}/>
+         <Route path="/signUp" element={<Signup/>}/>
          <Route path='/signIn' element={<SignIn/>}/>
          <Route path="/home" element={<Home/>}/>
          <Route path='/settings' element={<Settings/>}/>
