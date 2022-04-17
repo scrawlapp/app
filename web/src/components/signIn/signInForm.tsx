ed46db5f58ac4bcc6d162f2e3fd1ad2da9d9a3cf
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2022-04-17 23:17:55 +0530

    feat: changed password field type to password from text

--- web/src/components/signIn/signInForm.tsx
+++ web/src/components/signIn/signInForm.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../../style/auth.css";
 import { useForm } from "react-hook-form";
 import {useNavigate} from "react-router-dom";
+import PopUp from "../popUpWindow/popUp";
 
 
 function SignInForm(): JSX.Element{
    const {register, handleSubmit} = useForm();
    let statusCode: boolean = false;
    const navigate = useNavigate();
+   const [displayStyle, changeDisplay] = React.useState("none");
+   const [popUpMessage, setMessage] = React.useState("");
+   const color = "#e0490d";
 
    function onSubmit(data: any){
       localStorage.setItem('email', data.email);
@@ -32,20 +36,28 @@ function SignInForm(): JSX.Element{
          if (statusCode){
             navigate(`/home`);
          }
+         else {
+            console.log(data.message);
+            setMessage(data.message);
+            changeDisplay("inline-block");
+         }
       })
    }
 
 
    return(
-      <div className="FormContainer">
-         <form className = "form" onSubmit = {handleSubmit(onSubmit)}>
-            <br/><br/><br/><br/>
-            <input className = "formInput" id="emailInput" {...register("email", {required: true})} name="email"  placeholder="email"/>
-            <br/><br/>
-            <input className = "formInput" {...register("password", {required: true})} name="password" placeholder="password"/>
-            <br/><br/>
-            <button className = "formButton" type="submit"><strong>Sign In</strong></button> 
-         </form>
+      <div>
+         <div className="FormContainer">
+            <form className = "form" onSubmit = {handleSubmit(onSubmit)}>
+               <br/><br/><br/><br/>
+               <input className = "formInput" id="emailInput" {...register("email", {required: true})} name="email"  placeholder="email"/>
+               <br/><br/>
+               <input className = "formInput" {...register("password", {required: true})} type="password" name="password" placeholder="password"/>
+               <br/><br/>
+               <button className = "formButton" type="submit"><strong>Sign In</strong></button> 
+            </form>
+         </div>
+         <PopUp message={popUpMessage} display={displayStyle} changeDisplay={changeDisplay} color={color} />
       </div>
    );
 }
--- web/src/components/signUp/signUpForm.tsx
+++ web/src/components/signUp/signUpForm.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "../../style/auth.css";
 import { useForm } from "react-hook-form";
-
+import PopUp from "../popUpWindow/popUp";
 
 
 function SignUpForm(): JSX.Element{
 
    const {register, handleSubmit} = useForm();
 
+   const [message, setMessage] = React.useState("");
+   const [displayStyle, changeDisplay] = React.useState("none");
+   const [color, changeColor] = React.useState("#028a04");
 
    function onSubmit(data: any){
       fetch('/api/user/signup', {
@@ -22,28 +25,36 @@ function SignUpForm(): JSX.Element{
       .then(data => {
          console.log(data);
          if(data.message == "Created."){
-            console.log("Created");
+            setMessage("User Sign Up Successful");
+            changeColor("#028a04");
+            changeDisplay("inline-block");
          }
          else{
+            setMessage(data.message);
+            changeColor("#e0490d");
+            changeDisplay("inline-block");
          }
       })
    }
 
 
    return(
-      <div className="FormContainer">
-         <form className="form" onSubmit = {handleSubmit(onSubmit)}>
-         <br/><br/>
-            <input className = "formInput" {...register("firstName", {required: true})} name="firstName"  placeholder="first name"/>
-            <br/>
-            <input className = "formInput" {...register("lastName", {required: true})} name="lastName"  placeholder="last name"/>
-            <br/>
-            <input className = "formInput" {...register("email", {required: true})} name="email"  placeholder="email"/>
-            <br/>
-            <input className = "formInput" {...register("password", {required: true})} name="password" placeholder="password"/>
-            <br/>
-            <button className = "formButton" type="submit"><strong>Sign Up</strong></button> 
-         </form>
+      <div>
+         <div className="FormContainer">
+            <form className="form" onSubmit = {handleSubmit(onSubmit)}>
+               <br/><br/>
+               <input className = "formInput" {...register("firstName", {required: true})} name="firstName"  placeholder="first name" />
+               <br/>
+               <input className = "formInput" {...register("lastName", {required: true})} name="lastName"  placeholder="last name"/>
+               <br/>
+               <input className = "formInput" {...register("email", {required: true})} name="email"  placeholder="email"/>
+               <br/>
+               <input className = "formInput" {...register("password", {required: true})} type="password" name="password" placeholder="password"/>
+               <br/>
+               <button className = "formButton" type="submit"><strong>Sign Up</strong></button> 
+            </form>
+         </div>
+         <PopUp message={message} display={displayStyle} changeDisplay={changeDisplay} color={color} />
       </div>
    );
 }
