8c29c1526c761af4acfd8a8cdfdb7689ab19e50e
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2022-04-22 12:03:15 +0530

    style: landing page style changed

--- web/src/components/landingPage/landingPage.tsx
+++ web/src/components/landingPage/landingPage.tsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../../style/landingPage.css';
+import {useNavigate} from 'react-router-dom';
 
 function LandingPage(){
+  const navigate = useNavigate();
+
+  let featuresFirstChild = {
+    marginTop: "27px"
+  }
+
+  let featuresLastChild = {
+    marginBottom: "27px"
+  }
+
+  function getStarted() {
+    navigate(`/signUp`);
+  }
+
   return(
-    <section>
-      <article className='title'>
-    Scrawl
-  </article> 
-      
-  <article className='tag'>
-      Simple. Performant. Flexible.
-    </article>
-
-    <article className='desc'>
-        Scrawl lets you treat a document as a collection of blocks to hold anything.
-Combine text + data. Ditch your stack of doc + sheet.
-      </article>
-
-      <article className='points'>
-          <p>Supports Markdown ✅</p>
-          <p>Use it solo ✅</p>
-          <p>Collaborate with others ✅</p>
-          <p>Open source  ✅</p>
-        </article>
-
-        <article className='ghl'>
-            <p>Find us </p>
-            <p>on GitHub</p>
-            </article>
-
-        <article className='gs'>
-            <p>Get started🚀</p>
-            </article>
-    </section>
+    <div className='body'>
+      <h1 className='scrawlTitle'>Scrawl</h1>
+      <h3 className='subTitle'>Simple. Performant. Flexible</h3>
+      <p className='description'>Scrawl lets you treat a document as a collection of blocks to hold anything.<br/>
+Combine text + data. Ditch your stack of doc + sheet. </p>
+      <div className='featureBox'>
+      <p className='features' style={featuresFirstChild}>Supports Markdown</p><br/>
+      <p className='features'>Use it Solo</p><br/>
+      <p className='features'>Collaborate with others</p><br/>
+      <p className='features' style={featuresLastChild}>Open Source</p><br/>
+      </div>
+      <div className='buttons'>
+        <a className='landingPageButtons' href="https://github.com/scrawlapp">Find us on GitHub</a>
+        <button className='landingPageButtons' onClick={getStarted}>Get Started</button>
+      </div>
+    </div>
   )
 }
 
