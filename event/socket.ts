772db03e3c027d004d731c6fbcc0e3e7b385f829
viveknathani <viveknathani2402@gmail.com>
2022-04-25 12:12:48 +0530

    chore: describe the function

--- event/socket.ts
+++ event/socket.ts
@@ -1,5 +1,6 @@
 import { Server } from 'socket.io';
 
+// Describe how a page update message looks like
 interface PageUpdate {
 
     pageId: string,
@@ -7,10 +8,15 @@ interface PageUpdate {
     newValue: string
 }
 
+// This will attach events to the server side instance
+// of socket.io
 export function attachEvents(io: Server) {
 
     io.on('connection', (socket) => {
         socket.on('page diff', (diff: PageUpdate) => {
+            // Emit this page diff to all other clients.
+            // This is not ideal. A list of authorised clients
+            // should be maintained in the future. 
             io.emit('page diff', diff);
         });
     })
