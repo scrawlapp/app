cef62e4fb0bbbcc4fee095749c901a6b2b05e139
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2022-04-26 22:18:24 +0530

    style: modified page and popUp style

--- web/src/components/popUpWindow/popUp.tsx
+++ web/src/components/popUpWindow/popUp.tsx
@@ -17,15 +17,76 @@ function PopUp (props: any): JSX.Element {
       console.log(popUpStyle);
       props.changeDisplay("none");
    }
-
-   return (
-      <div className='popUpBackground' style={popUpStyle}>
-         <div className='popUpCard'>
-            <h3 className='popUpMessage' style={h3Color}>{props.message}</h3>
-            <button className='popUpButton' onClick={ok}>Ok</button>
+   if (props.message == "anger") {
+      return (
+         <div className='popUpBackground' style={popUpStyle}>
+            <div className='popUpCard'>
+               <h3 className='popUpMessage' style={h3Color}>{props.message}<br/>😠</h3>
+               <button className='popUpButton' onClick={ok}>Ok</button>
+            </div>
          </div>
-      </div>
-   )
+      )
+   }
+   else if (props.message == "love") {
+      return (
+         <div className='popUpBackground' style={popUpStyle}>
+            <div className='popUpCard'>
+               <h3 className='popUpMessage' style={h3Color}>{props.message}<br/>💖</h3>
+               <button className='popUpButton' onClick={ok}>Ok</button>
+            </div>
+         </div>
+      )
+   }
+   else if (props.message == "sadness") {
+      return (
+         <div className='popUpBackground' style={popUpStyle}>
+            <div className='popUpCard'>
+               <h3 className='popUpMessage' style={h3Color}>{props.message}<br/>😔</h3>
+               <button className='popUpButton' onClick={ok}>Ok</button>
+            </div>
+         </div>
+      )
+   }
+   else if (props.message == "joy") {
+      return (
+         <div className='popUpBackground' style={popUpStyle}>
+            <div className='popUpCard'>
+               <h3 className='popUpMessage' style={h3Color}>{props.message}<br/>😄</h3>
+               <button className='popUpButton' onClick={ok}>Ok</button>
+            </div>
+         </div>
+      )
+   }
+   else if (props.message == "fear") {
+      return (
+         <div className='popUpBackground' style={popUpStyle}>
+            <div className='popUpCard'>
+               <h3 className='popUpMessage' style={h3Color}>{props.message}<br/>😨</h3>
+               <button className='popUpButton' onClick={ok}>Ok</button>
+            </div>
+         </div>
+      )
+   }
+   else if (props.message == "surprise") {
+      return (
+         <div className='popUpBackground' style={popUpStyle}>
+            <div className='popUpCard'>
+               <h3 className='popUpMessage' style={h3Color}>{props.message}<br/>😲</h3>
+               <button className='popUpButton' onClick={ok}>Ok</button>
+            </div>
+         </div>
+      )
+   }
+   else { 
+      return (
+         <div className='popUpBackground' style={popUpStyle}>
+            <div className='popUpCard'>
+               <h3 className='popUpMessage' style={h3Color}>{props.message}</h3>
+               <button className='popUpButton' onClick={ok}>Ok</button>
+            </div>
+         </div>
+      )
+   }
 }
 
 export default PopUp;
