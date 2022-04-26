c006c335896ce41eee6f0dd887df55f7ec5a97df
viveknathani <viveknathani2402@gmail.com>
2022-04-27 00:06:29 +0530

    chore: clean deps, separate network calls

--- web/src/components/page/page.tsx
+++ web/src/components/page/page.tsx
@@ -1,114 +1,32 @@
 import React from 'react';
-import ContentEditable, { ContentEditableEvent } from 'react-contenteditable';
-import { Block } from '../block/block';
+import { Block, HeadingBlock, ContentEditableEvent } from '../block/block';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import "../../styles/page.css"
+import { BlockStructure, 
+    insertAnEmptyBlock, 
+    updateBlock, 
+    fetchAllBlocks, 
+    deleteBlock } from './api';
+import '../../styles/page.css';
 
 // describe the props to be passed into Page
 export interface PageProps {
 
-    pageId: string,
-    pageName: string,
-    updatePageName: (pageId: string, name: string) => void,
+    pageId: string
+    pageName: string
+    updatePageName: (pageId: string, name: string) => void
     deletePage: (pageId: string) => void
-    shared: boolean,
+    shared: boolean
     ability: string
 }
 
-// describe how the block looks like during network calls
-export interface BlockStructure {
-
-    id: string,
-    pageId: string,
-    tag: string,
-    html: string,
-    position: number,
-    href: string | null,
-    src: string | null,
+const stylePageButton = {
+    marginLeft: "3%",
+    marginBottom: "15px",
+    marginTop: "10px"
 }
 
-// POST /api/block/
-function insertAnEmptyBlock(pageId: string, position: number) {
-
-    if (pageId.length < 32) {
-        return;
-    }
-
-    fetch('/api/block/', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            pageId,
-            tag: 'p',
-            html: '',
-            position,
-            src: null,
-            href: null
-        })
-    })
-    .then((response) => console.log(response))
-    .catch((err) => console.log(err));
-} 
-
-// PUT /api/block/
-function updateBlock(block: any) {
-    
-    fetch(`/api/block/`, {
-        method: 'PUT',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(block)
-    })
-    .then((response) => console.log(response))
-    .catch((err) => console.log(err));
-}
-
-// GET /api/block/all/:pageId
-function fetchAllBlocks(pageId: string, setBlocks: React.Dispatch<React.SetStateAction<BlockStructure[]>>) {
-
-    fetch(`/api/block/all/${pageId}`, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    }).then(response => response.json())
-    .then(data => {
-        if (data.length === 0) {
-            insertAnEmptyBlock(pageId, 0);
-            fetchAllBlocks(pageId, setBlocks);
-            return;
-        }
-        data.sort((a: BlockStructure, b: BlockStructure): number => {
-            return a.position - b.position;
-        });
-        setBlocks(data);
-    }).catch((err) => {
-        console.log(err);
-    });
-}
-
-// DELETE /api/block/
-function deleteBlock(blockId: string) {
-
-    fetch(`/api/block/`, {
-        method: 'DELETE',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({id: blockId})
-    })
-    .then((response) => console.log(response))
-    .catch((err) => console.log(err));
-}
-
+// check if block has no content
 function isAnEmptyBlock(block: BlockStructure): boolean {
 
     return (
@@ -119,53 +37,48 @@ function isAnEmptyBlock(block: BlockStructure): boolean {
     )
 }
 
+// fire when the page name changes
+function handlePageNameChange(event: ContentEditableEvent, 
+    props: PageProps, pageNameRef: React.MutableRefObject<string>) {
+
+    if (props.shared && props.ability !== 'editor') {
+        return;
+    }
+    if (event !== null) {
+        pageNameRef.current = event.target.value;
+    }
+}
+
+// fire when the page name block goes out of focus
+function handlePageNameBlur(props: PageProps, 
+    pageNameRef: React.MutableRefObject<string>): 
+    React.FocusEventHandler<HTMLDivElement> | undefined {
+
+    if (props.shared && props.ability !== 'editor') {
+        return;
+    }
+    props.updatePageName(props.pageId, pageNameRef.current);
+}
+
 // the Page component
 export function Page(props: PageProps): JSX.Element {
 
     const [blocks, setBlocks] = React.useState<BlockStructure[]>([]);
     const pageNameRef = React.useRef(props.pageName);
     const [deletePageCue, deletePageCueCaller] = React.useState<number>(0);
-    const pageButton = {
-        marginLeft: "3%",
-        marginBottom: "15px",
-        marginTop: "10px"
-    }
-
+    
     // we fetch all blocks only when pageId changes
     // or the Page component is mounted onto the DOM
     React.useEffect(() => {
         fetchAllBlocks(props.pageId, setBlocks);
     }, [props.pageId]);
 
+    // when page is deleted, blocks need to be emptied
     React.useEffect(() => {
         setBlocks([]);
     }, [deletePageCue]);
 
-    function handlePageNameChange(event: ContentEditableEvent) {
-
-        if (props.shared && props.ability !== 'editor') {
-            return;
-        }
-        if (event !== null) {
-            pageNameRef.current = event.target.value;
-        }
-    }
-
-    function handlePageNameBlur() {
-
-        if (props.shared && props.ability !== 'editor') {
-            return;
-        }
-        props.updatePageName(props.pageId, pageNameRef.current);
-    }
-
-    // const stylePageName = {
-    //     textAlign: 'left',
-    //     fontSize: '32px',
-    //     fontWeight: 'bold',
-    //     margin: '25px 25px'
-    // }
-
+    // case where no page is selected
     if (blocks.length === 0) {
         return(
             <div>
@@ -175,32 +88,41 @@ export function Page(props: PageProps): JSX.Element {
         );
     }
 
+    // case where a page does not have any blocks made by user
     if (blocks.length === 1 && isAnEmptyBlock(blocks[blocks.length - 1])) {
         const blocksCopy = blocks;
         blocksCopy[blocksCopy.length - 1].html = 'click here to begin';
         setBlocks(blocksCopy);
     }
 
+    // return heading + delete button + collection of blocks
     return(
         <div>
-
-            <ContentEditable
-                html={props.pageName}
-                tagName='p'
-                onChange={handlePageNameChange}
-                onBlur={handlePageNameBlur}
-                className="pageTitle"
-                // style={stylePageName}
-            ></ContentEditable>
-
-            
-            <button onClick={() => {
-                if (props.shared && props.ability !== 'editor') {
+            <HeadingBlock
+                onChange={ function (event: ContentEditableEvent) {
+                    handlePageNameChange(event, props, pageNameRef)
+                }}
+                onBlur={function (): React.FocusEventHandler<HTMLDivElement> | undefined {
+                    handlePageNameBlur(props, pageNameRef);
                     return;
-                }
-                props.deletePage(props.pageId);
-                deletePageCueCaller(deletePageCue + 1);
-            }} className="iconButton" style={pageButton}><FontAwesomeIcon className="icons" icon={faTrash} /></button>
+                }}
+                tagName='p'
+                className="pageTitle"
+                html={props.pageName}
+            />
+
+            <button 
+                onClick={() => {
+                    if (props.shared && props.ability !== 'editor') {
+                        return;
+                    }
+                    props.deletePage(props.pageId);
+                    deletePageCueCaller(deletePageCue + 1);
+                }} 
+                className="iconButton" 
+                style={stylePageButton}>
+                    <FontAwesomeIcon className="icons" icon={faTrash}/>
+                </button>
 
             {blocks.map((block, index) => (
                 <Block 
@@ -221,8 +143,6 @@ export function Page(props: PageProps): JSX.Element {
                     canEdit={!props.shared || (props.shared && props.ability === 'editor')}
                 />)
             )}
-
-
         </div>
     )
 }
